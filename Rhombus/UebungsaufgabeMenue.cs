ce99d8ef40877c81d04102f393bdcb08d67aade8
Fuchs <hoyerjason0@gmail.com>
2022-01-14 17:37:57 +0100

    Menü hinzugefüght

---
+++ Rhombus/UebungsaufgabeMenue.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Rhombus
+{
+    public partial class UebungsaufgabeMenue : Form
+    {
+        public UebungsaufgabeMenue()
+        {
+            InitializeComponent();
+        }
+    }
+}

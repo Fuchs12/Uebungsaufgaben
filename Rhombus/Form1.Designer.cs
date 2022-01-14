fd35cdb6f26ebe3f344135de13096bdeb33d2094
Fuchs <hoyerjason0@gmail.com>
2022-01-14 17:03:13 +0100

    Paint Methode ergänzt, so das sie nun das Rhombus kerrekt ausgibt.

--- Rhombus/Form1.Designer.cs
+++ Rhombus/Form1.Designer.cs
@@ -29,10 +29,20 @@ namespace Rhombus
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(813, 445);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- Rhombus/Form1.cs
+++ Rhombus/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Drawing.Drawing2D;
 
 namespace Rhombus
 {
@@ -17,5 +18,35 @@ namespace Rhombus
             InitializeComponent();
         }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        Pen blackPen = new Pen(Color.Black);
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics graphics = e.Graphics;
+            graphics.SmoothingMode = SmoothingMode.HighQuality;
+
+            Rectangle rectangle = new Rectangle(10 ,10,110,110);
+            
+            Point obenMitte = new Point(rectangle.X + rectangle.Width/2 ,rectangle.Y);
+            Point untenMitte = new Point(rectangle.X + rectangle.Width/2,rectangle.Height + rectangle.Y);
+            Point rechtsMitte = new Point(rectangle.Width + rectangle.X, rectangle.Y + rectangle.Height/2);
+            Point linksMitte = new Point(rectangle.X, rectangle.Y + rectangle.Height/2);
+
+            graphics.DrawRectangle(blackPen, rectangle);
+            
+            GraphicsPath path = new GraphicsPath();
+            path.AddLine(obenMitte, linksMitte);
+            path.AddLine(linksMitte, untenMitte);
+            path.AddLine(untenMitte, rechtsMitte);
+            path.AddLine(rechtsMitte, obenMitte);
+            path.CloseFigure();
+
+            graphics.DrawPath(blackPen, path);
+        }
     }
 }
